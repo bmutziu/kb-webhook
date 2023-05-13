613da1268b1f957fa1d327faa29abe12b0c54f87
Bogdan Muțiu <bmutziu@gmail.com>
2023-05-13 14:18:33 +0000

    repair rbac

--- internal/controller/app_controller.go
+++ internal/controller/app_controller.go
@@ -44,6 +44,9 @@ type AppReconciler struct {
 //+kubebuilder:rbac:groups=app.bmutziu.me,resources=apps/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=app.bmutziu.me,resources=apps/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
